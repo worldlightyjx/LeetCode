881a3d7fac6bfe31c6b2ba81ee4aa5f185f2acb6
worldlightyjx <worldlightyjx@qq.com>
2022-02-23 22:42:20 +0800

    add 25

--- Solutions/Linked List/25. Reverse Nodes in k-Group/Solution.java
+++ Solutions/Linked List/25. Reverse Nodes in k-Group/Solution.java
@@ -1,15 +1,38 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        for (ListNode p = dummy;;) {
+            ListNode q = p;
+            for (int i = 0; i < k && q != null; i++) {
+                q = q.next;
+            }
+            if (q == null)
+                break;
+            ListNode a = p.next;
+            ListNode b = a.next;
+            for (int i = 0; i < k - 1; i++) {
+                ListNode tmp = b.next;
+                b.next = a;
+                a = b;
+                b = tmp;
+            }
+            ListNode c = p.next;
+            c.next = b;
+            p.next = a;
+            p = c;
+        }
+        return dummy.next;
+
     }
 }
