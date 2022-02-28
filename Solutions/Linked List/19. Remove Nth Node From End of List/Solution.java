a032bc3949ed00553976171cac40c79e1becfbca
worldlightyjx <worldlightyjx@qq.com>
2022-02-28 22:32:26 +0800

    add 19 92

--- Solutions/Linked List/19. Remove Nth Node From End of List/Solution.java
+++ Solutions/Linked List/19. Remove Nth Node From End of List/Solution.java
@@ -1,15 +1,29 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        
+    public ListNode removeNthFromEnd(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        int num = 0;
+        for (ListNode p = dummy; p != null; p = p.next) {
+            num++;
+        }
+        ListNode p = dummy;
+        for (int i = 0; i < num - k - 1; i++) {
+            p = p.next;
+
+        }
+        p.next = p.next.next;
+        return dummy.next;
+
     }
 }
--- Solutions/Linked List/92. Reverse Linked List II/Solution.java
+++ Solutions/Linked List/92. Reverse Linked List II/Solution.java
@@ -1,23 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(left==right) return head;
+        if (left == right)
+            return head;
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
         ListNode p = dummy;
-        for(int i=0; i<m-1;i++){
+        for (int i = 0; i < left - 1; i++) {
             p = p.next;
         }
-        ListNode 
-        
+        ListNode a = p;
+        ListNode b = a.next;
+        ListNode c = b.next;
+        for (int i = left; i < right; i++) {
+            ListNode tmp = c.next;
+            c.next = b;
+            b = c;
+            c = tmp;
+
+        }
+        a.next.next = c;
+        a.next = b;
+        return dummy.next;
+
     }
 }
