676f82c9cf758d2ea172a2d4280437854b00b0df
worldlightyjx <worldlightyjx@qq.com>
2022-07-07 00:02:52 +0800

    Update 424 1004

--- Solutions/Sliding Window/1004. Max Consecutive Ones III/Solution.java
+++ Solutions/Sliding Window/1004. Max Consecutive Ones III/Solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        
+        int windowStart =0, maxOnes = 0, maxLength = 0;
+        for(int windowEnd = 0; windowEnd<nums.length;windowEnd++){
+            if(nums[windowEnd]==1){
+                maxOnes++;
+            }
+
+            if(windowEnd-windowStart+1-maxOnes>k){
+                if(nums[windowStart]==1){
+                    maxOnes--;
+                }
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd-windowStart+1);
+        }
+        return maxLength;
     }
 }
--- Solutions/Sliding Window/424. Longest Repeating Character Replacement/Solution.java
+++ Solutions/Sliding Window/424. Longest Repeating Character Replacement/Solution.java
@@ -1,9 +1,23 @@
+import java.util.Map;
+
 class Solution {
     public int characterReplacement(String s, int k) {
         int wStart = 0, maxLength=0, maxRepeatCount = 0;
         Map<Character,Integer> frequencyMap = new HashMap<>();
-        for(int wEnd = 0; wEnd<s.length;wEnd++){
-            char
+        for(int wEnd = 0; wEnd<s.length();wEnd++){
+            char rightChar = s.charAt(wEnd);
+            frequencyMap.put(rightChar, frequencyMap.getOrDefault(rightChar,0)+1);
+
+            maxRepeatCount = Math.max(maxRepeatCount, frequencyMap.get(rightChar));
+
+            if(wEnd-wStart+1-maxRepeatCount>k){
+                frequencyMap.put(s.charAt(wStart), frequencyMap.get(s.charAt(wStart))-1);
+                wStart++;
+                
+            }
+
+            maxLength = Math.max(maxLength, wEnd-wStart+1);
         }
+        return maxLength;
     }
 }
