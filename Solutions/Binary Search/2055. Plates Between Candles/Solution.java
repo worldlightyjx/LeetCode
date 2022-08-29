2323941b237afe0a11ec230da414d772eb6db112
worldlightyjx <worldlightyjx@qq.com>
2022-08-30 00:04:31 +0800

    Add 2055 Binary Search And Prefix Sum

--- Solutions/Binary Search/2055. Plates Between Candles/Solution.java
+++ Solutions/Binary Search/2055. Plates Between Candles/Solution.java
@@ -4,11 +4,14 @@ import java.util.List;
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
         int n = s.length();
+        //prefix sum to record the sum of plates on every position
         int[] sum = new int[n + 1];
+        //using a list to record the index of every candle in the array
         List<Integer> list = new ArrayList<>();
         int m = queries.length;
         int[] res = new int[m];
 
+        //record list and prefix sum
         for (int i = 0; i < n; i++) {
             if (s.charAt(i) == '|')
                 list.add(i);
@@ -18,6 +21,8 @@ class Solution {
             return res;
         }
 
+        //For every query, first using binary search to find the most left candle, 
+        //Then the same method to find the most right candle
         for (int i = 0; i < m; i++) {
             int a = queries[i][0], b = queries[i][1];
             int c = -1, d = -1;
@@ -58,7 +63,7 @@ class Solution {
                 d = list.get(r);
             else
                 continue;
-
+            
             if (c <= d)
                 res[i] = (sum[d + 1] - sum[c]);
 
