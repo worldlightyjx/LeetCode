ed25c78870e8e65dfe40371056cf35f06863276f
Yang <worldlightyjx@qq.com>
2026-04-06 18:08:25 +0800

    add solution for 371. Sum of Two Integers and refactor 121. Best Time to Buy and Sell Stock

--- Solutions/Dynamic Programming/121. Best Time to Buy and Sell Stock/Solution.java
+++ Solutions/Dynamic Programming/121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,18 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        if(n==0){
-            return 0;
-        }
-        int maxProfit = 0;
         int minPrice = prices[0];
-
-        for(int i=0;i<n;i++){
-            maxProfit = Math.max(maxProfit,prices[i]-minPrice);
-            minPrice = Math.min(minPrice, prices[i]);
+        int ans = 0;
+        for (int num : prices) {
+            ans = Math.max(ans, num - minPrice);
+            minPrice = Math.min(minPrice, num);
         }
+        return ans;
 
-        return maxProfit;
-        
     }
 }
