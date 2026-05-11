8be758c1d99e35a30c46dd232ee8fc3b4c85e860
Yang <worldlightyjx@qq.com>
2026-05-11 23:08:36 +0800

    finish binary search template

--- Solutions/Binary Search/34. Find First and Last Position of Element in Sorted Array/Solution.java
+++ Solutions/Binary Search/34. Find First and Last Position of Element in Sorted Array/Solution.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int start = lowerBound(nums, target);
+        if (start == nums.length || nums[start] != target) {
+            return new int[] { -1, -1 };
+        }
 
+        int end = lowerBound(nums, target + 1) - 1;
+        return new int[] { start, end };
     }
 
     private int lowerBound(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
+        int l = -1;
+        int r = nums.length;
+        while (l + 1 < r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] >= target) {
-                r = mid - 1;
+                r = mid;
             } else {
-                l = mid + 1;
+                l = mid;
             }
         }
-        return l;
+        return r;
     }
 }
